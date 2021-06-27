66bc7776c7838ac2a36b9217540bb81298f479ba
Mayank <mayankguptacse029@gmail.com>
2021-06-27 20:43:17 +0530

    MVC Completed

--- Routes/studentRouter.js
+++ Routes/studentRouter.js
@@ -1,18 +1,9 @@
 const express  = require('express');
 const router   = express.Router();
-const Student = require('../models/students')
+const studentController = require('../controllers/studentController')
 
+router.get('/',studentController.student_get);
 
-router.get('/',(req, res) =>{
-    Student.find()
-    .then(data => res.status(200).json({message:"all students fetched",data:data}))
-    .catch(err => res.status(404).json({error:err.message || err.tostring()}))
-});
-
-router.post('/',(req, res) => {
-    const students = new Student(req.body);
-    students.save().then((data) => res.status(200).json({message:'students saved',data})) 
-    .catch(err => res.status(404).json({error:err.message}))
-});
+router.post('/',studentController.student_post);
 
 module.exports = router;
---
+++ controllers/studentController.js
@@ -0,0 +1,15 @@
+const Student = require('../models/students')
+
+const student_get =  (req,res) =>{
+    Student.find()
+    .then(data => res.status(200).json({message:"all students fetched",data:data}))
+    .catch(err => res.status(404).json({error:err.message || err.tostring()}))
+}
+
+const student_post  = (req,res) => {
+    const students = new Student(req.body);
+    students.save().then((data) => res.status(200).json({message:'students saved',data})) 
+    .catch(err => res.status(404).json({error:err.message}))
+}
+
+module.exports = {student_get, student_post}

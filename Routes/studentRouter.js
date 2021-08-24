ec9dff1b5181b7b3995b95fd5482e8ea98f5b9b2
Mayank <mayankguptacse029@gmail.com>
2021-08-24 18:15:38 +0530

    hello

--- Routes/studentRouter.js
+++ Routes/studentRouter.js
@@ -4,6 +4,7 @@ const studentController = require('../controllers/studentController')
 
 router.get('/',studentController.student_get);
 
+// this is second
 router.post('/',studentController.student_post);
 
 module.exports = router; 

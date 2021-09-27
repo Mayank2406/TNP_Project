59e5daf8e68ef744777a6f4ff803a50a8d6c3f6e
mayank <mayankguptacse029@gmail.com>
2021-09-27 15:41:12 +0530

    Minor Chnges to remove conflicts

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,17 +1,9 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: { type: String, required: true },
-    password: { type: String, required: true}
-}, { timestamps: true });
-
-const User = mongoose.model('DemoUser', userSchema);
-=======
-const Schema   = mongoose.Schema;
-
-const userSchema = new Schema({
+    password: { type: String, required: true},
     secret:{type:String,required:true},
     userID:{type:Number, required:true},
     userName:{type:String, required:true},
@@ -23,6 +15,5 @@ const userSchema = new Schema({
 
 
 const User = mongoose.model('User',userSchema);
->>>>>>> f055e32cabf17606721d959d0d8a9d8e65fbb7e4
 
 module.exports = User;
